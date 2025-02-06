f585e785ab99225c7a1550420268b46952f1c438
Your Name <farhamostafa657@gmail.com>
2025-02-07 00:13:15 +0400

    show list of friends friends and friend component

--- eaten-split/src/App.js
+++ eaten-split/src/App.js
@@ -1,5 +1,58 @@
-function App() {
-  return <div>Hello</div>;
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+export default function App() {
+  const friends = initialFriends;
+  return (
+    <div className="sideBar">
+      <ul>
+        {friends.map((friend) => (
+          <Friend friend={friend} key={friend.name} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App;
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name}></img>
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          you owe {friend.name} {Math.abs(friend.balance)}
+        </p>
+      )}
+
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {friend.balance}
+        </p>
+      )}
+
+      {friend.balance === 0 && <p>you and {friend.name} are evenly</p>}
+
+      <button>Select</button>
+    </li>
+  );
+}
